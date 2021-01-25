1f90ddd235cd482a99cb3be9f21bb760bd59739c
pragun <pragun.goyal@gmail.com>
2021-01-25 15:46:07 -0500

    Splits up the identifier_association_cmd into its own module

---
+++ common_defs.py
@@ -0,0 +1,5 @@
+number_of_key_style_identifiers = 10
+default_key_style_identifiers = ['1u', '1.25u', '1.5u', '1.75u', '2u', '2.25u', '2.5u', '2.75u', '3u']
+
+attribute_group_name = 'mechKbrdTools'
+identifiers_attrib_name = 'key_styles'
