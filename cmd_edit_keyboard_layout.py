a8ec19792f4f2afbd4dcc8b4189de7f09d1933ce
pragun <pragun.goyal@gmail.com>
2021-01-27 16:05:43 -0500

    Generates equivalent looking sketches for layouts

--- cmd_edit_keyboard_layout.py
+++ cmd_edit_keyboard_layout.py
@@ -8,13 +8,77 @@ import json, re
 from .common_defs import *
 from . import hjson
 import html
+from collections import OrderedDict
+from .keyboard_layout import expand_layout_list
+import pprint
+import math
+
 # Event handler for the commandCreated event.
 
 layout_text_box_id = 'layout_text_box_id'
 unit_size_id = 'unit_size_id'
 validate = 'validate_button_id'
+parse_results_text_box_id = 'parse_results_text_box_id'
+sketch_input_id = 'sketch_plane_selection_id'
 
 handlers = []
+
+def check_high_level_structure(layout_input):
+    #Expectations for input: 
+    #1. A List of Lists
+    r = 0
+    if not isinstance(layout_input,list):
+        raise ValueError("Expected a list for the layout input.")
+    for row in layout_input:
+        r = r+1
+        if not isinstance(row, list):
+            raise ValueError("Expected a list of lists for the layout input.\nRow:%d"%(r,))
+        i = 0
+        for item in row:
+            i = i+1
+            if not (isinstance(item, OrderedDict) or isinstance(item, str)):
+                raise ValueError("Expected either a string or a dictionary value.\nRow:%d,Item:%s"%(r,item))
+
+
+
+def parse_layout_input(input_text, parse_results_text_box=None):
+    def put_parse_msg(msg):
+        if parse_results_text_box is not None:
+            parse_results_text_box.text = msg
+
+    input_lines = str.split(input_text,'\n')
+    #print(input_lines)
+
+    input_text_except_last_line = ''
+    if len(input_lines) > 0:
+        input_text_except_last_line = '\n'.join(input_lines[0:-1])
+    #print(input_text_except_last_line)
+
+    input_text_except_last_line.strip()
+
+    hjson_parsed_object = None
+    
+    if input_text_except_last_line != '':
+        try:
+            hjson_parsed_object = hjson.loads(input_text)
+            check_high_level_structure(hjson_parsed_object)
+            put_parse_msg('Parsed OK')
+            print('Parsed as:',hjson_parsed_object)
+        except Exception as e:
+            print("Couldn't parse as a hjson object, will try to parse as a list next..")
+            print(e)
+            input_text = '['+input_text+']'
+            try:
+                hjson_parsed_object = hjson.loads(input_text)
+                check_high_level_structure(hjson_parsed_object)
+                put_parse_msg('Parsed OK as list')
+                print('Parsed as:',hjson_parsed_object)
+            except Exception as e:
+                put_parse_msg('Exception Parsing as list: %s'%(e,))
+                print(e)
+
+    return hjson_parsed_object
+
 
 class EditKeyboardLayoutCreatedEventHandler(adsk.core.CommandCreatedEventHandler):
     def __init__(self):
@@ -36,6 +100,10 @@ class EditKeyboardLayoutCreatedEventHandler(adsk.core.CommandCreatedEventHandler
         default_u_size = adsk.core.ValueInput.createByString('19.05mm')
         unit_size_box_input  = inputs.addDistanceValueCommandInput(unit_size_id, 'Size of 1u', default_u_size)
         layout_text_box_input = inputs.addTextBoxCommandInput(layout_text_box_id,'Keyboard Layout \n(as used at http://www.keyboard-layout-editor.com/)','',15,False)
+        parse_results_text_box = inputs.addTextBoxCommandInput(parse_results_text_box_id,'Parse Results:', '', 3, True)
+        sketch_input = inputs.addSelectionInput(sketch_input_id,'Select Sketch to Add Generated Keyboard Layout Onto', 'Select a sketch to create a keyboard layout sketch into')
+        sketch_input.addSelectionFilter('Sketches')
+        sketch_input.setSelectionLimits(0,1)
 
         # Connect to the execute event.
         #onActivateInputs = SelectSingleKeyComponentsActivateHandler()
@@ -58,6 +126,54 @@ class EditKeyboardLayoutExecuteHandler(adsk.core.CommandEventHandler):
     def notify(self, args):
         print("In Execute:")
         app = adsk.core.Application.get()
+        eventArgs = adsk.core.CommandEventArgs.cast(args)
+        inputs = eventArgs.firingEvent.sender.commandInputs
+
+        parse_results_text_box = inputs.itemById(parse_results_text_box_id)
+        layout_input_text = inputs.itemById(layout_text_box_id)
+        unit_size_input = inputs.itemById(unit_size_id)
+        sketch_input = inputs.itemById(sketch_input_id)
+        input_text = html.unescape(layout_input_text.text)
+
+        unit_value = unit_size_input.value
+
+        selected_sketch = sketch_input.selection(0).entity
+
+        print(selected_sketch)
+        parsed_layout = parse_layout_input(input_text)
+        print('Parsed Layout: \n',parsed_layout)
+        expanded_layout = expand_layout_list(parsed_layout)
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(expanded_layout)
+
+        sketchPoints = selected_sketch.sketchPoints
+        lines = selected_sketch.sketchCurves.sketchLines;
+
+        for item in expanded_layout:
+            angle_rad = math.radians(item['r'])
+            x = unit_value * (item['rx'] + (item['x']*math.cos(angle_rad) - item['y']*math.sin(angle_rad)))
+            y = unit_value * (item['ry'] + (item['y']*math.cos(angle_rad) + item['x']*math.sin(angle_rad)))
+
+            corner_point = adsk.core.Point3D.create(x, -y, 0)
+            del_x = item['w']*math.cos(angle_rad)
+            del_y = item['w']*math.sin(angle_rad)
+
+            x = x + (unit_value*del_x)
+            y = y + (unit_value*del_y)
+
+            adjacent_top_point = adsk.core.Point3D.create(x, -y, 0)
+
+            del_x = -item['h']*math.sin(angle_rad)
+            del_y = item['h']*math.cos(angle_rad)
+            x = x + (unit_value*del_x)
+            y = y + (unit_value*del_y)
+            lower_diagnoal_point = adsk.core.Point3D.create(x, -y, 0)
+
+            recLines = lines.addThreePointRectangle( corner_point,
+                                adjacent_top_point,
+                                lower_diagnoal_point)
+            
+
 
 class EditKeyboardLayoutValidateInputsHandler(adsk.core.ValidateInputsEventHandler):
     def __init__(self):
@@ -69,26 +185,14 @@ class EditKeyboardLayoutValidateInputsHandler(adsk.core.ValidateInputsEventHandl
         eventArgs = adsk.core.ValidateInputsEventArgs.cast(args)
         inputs = eventArgs.firingEvent.sender.commandInputs
 
+        eventArgs.areInputsValid = False
+
         inputs_valid = True 
-
+        parse_results_text_box = inputs.itemById(parse_results_text_box_id)
         layout_input_text = inputs.itemById(layout_text_box_id)
+        sketch_input = inputs.itemById(sketch_input_id)
         input_text = html.unescape(layout_input_text.text)
-        input_lines = str.split(input_text,'\n')
-        print(input_lines)
-
-        input_text_except_last_line = ''
-        if len(input_lines) > 0:
-            input_text_except_last_line = '\n'.join(input_lines[0:-1])
-        print(input_text_except_last_line)
-
-        input_text_except_last_line.strip()
-        if input_text_except_last_line != '':
-            try:
-                a = hjson.loads(input_text)
-                print('Im here')
-                print(a)
-            except Exception as e:
-                print('Or Im here')
-                print(e)
-
-        eventArgs.areInputsValid = inputs_valid
+        hjson_parsed_object = parse_layout_input(input_text,parse_results_text_box=parse_results_text_box)
+ 
+        if sketch_input.selectionCount > 0 and hjson_parsed_object != None:
+            eventArgs.areInputsValid = True
---
+++ keyboard_layout.py
@@ -0,0 +1,70 @@
+# Author-Pragun Goyal 
+# Description-This command creates/edits an attribute saved on the 
+# main design object containing a JSON object for the Keyboard-Layout
+# in the same format as used/specified here http://www.keyboard-layout-editor.com/
+
+from collections import OrderedDict
+
+class KeyParameters(object):
+    def __init__(self):
+        self.keys = ['rx','ry','r','x','y','w','h']
+        for key in self.keys:
+            setattr(self,key,0)
+
+        self.w = 1
+        self.h = 1
+
+    def reset_for_new_row(self):
+        self.y += 1
+        self.x = 0
+        self.w = 1
+        self.h = 1
+
+    def check_add(self,key,i_dict):
+        if key in i_dict:
+            value = getattr(self,key) + i_dict[key]
+            setattr(self,key,value)
+
+    def check_set(self,key,i_dict):
+        if key in i_dict:
+            setattr(self,key,i_dict[key])
+
+    def process_input(self,i_dict):
+        for i in ['x','y']:
+            self.check_add(i, i_dict)
+
+        for i in ['rx','ry','r']:
+            if i in i_dict:
+                self.y = 0
+                
+        for i in ['rx','ry','r','w','h']:
+            self.check_set(i,i_dict)
+            
+    
+    def generate_key_here(self,name):
+        o_dict = {}
+        for key in self.keys:
+            o_dict[key] = getattr(self,key)       
+        o_dict['id'] = str(name)
+
+        self.x += self.w
+        self.w = 1
+        self.h = 1
+        
+        return o_dict
+ 
+
+def expand_layout_list(rows):
+    key_params = KeyParameters()
+    keys = []
+
+    for row in rows:
+        for item in row:
+            if isinstance(item,OrderedDict):
+                key_params.process_input(item)
+            else:
+                keys.append(key_params.generate_key_here(item))
+
+        key_params.reset_for_new_row()
+    
+    return keys
